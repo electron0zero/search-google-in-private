141c0747ad29038fd3baab49e165271740d898f7
Suraj Nath <devversitymail@gmail.com>
2017-10-11 01:01:37 +0530

    working on chrome

--- chrome/background.js
+++ chrome/background.js
@@ -1,46 +1,18 @@
 /*
-Called when the item has been created, or when creation failed due to an error.
-We'll just log success/failure here.
-*/
-function onCreated() {
-  if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
-  } else {
-    // console.log("Item created successfully");
-  }
-}
-
-/*
-Called when the item has been removed.
-We'll just log success here.
-*/
-function onRemoved() {
-  // console.log("Item removed successfully");
-}
-
-/*
-Called when there was an error.
-We'll just log the error here.
-*/
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-/*
 Create all the context menu items.
 */
-browser.menus.create({
+chrome.contextMenus.create({
   id: "selected-text",
-  title: browser.i18n.getMessage("menuItemSelection"),
-  contexts: ["selection"]
-}, onCreated);
+  title: chrome.i18n.getMessage("menuItemSelection"),
+  contexts: ["selection"],
+});
 
 
 /*
 The click event listener, where we perform the appropriate action given the
 ID of the menu item that was clicked.
 */
-browser.menus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "selected-text":
       // create a incognito window
@@ -49,7 +21,7 @@ browser.menus.onClicked.addListener((info, tab) => {
           incognito: true,
           url: [searchURL]
         };
-      let creating = browser.windows.create(createData);
+      let creating = chrome.windows.create(createData);
       creating.then(() => {
         console.log("The incognito window has been created");
       });
